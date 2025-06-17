36f9e2f52c4e2746f5f08d2e03a3063b8fd7a15f
SRINU NAYAK <srinunayakk7@gmail.com>
2025-06-17 18:51:51 -0400

    added setup file

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # End to End Project
 
 1. create the virtual environment (conda create -p venv/ python==3.10 (version)) and activate it (conda activate venv/)
-2. 
+2. add the requirements.txt file to install packages (python install -r requirements.txt)
+3. add the setup.py file
+4. 
---
+++ setup.py
@@ -0,0 +1,17 @@
+from typing import List
+from setuptools import setup, find_packages
+
+def get_requirements(filename: str) -> List[str]:
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    author='srinu',
+    author_email='srinunayakk7@gmail.com',
+    install_requires= get_requirements('requirements.txt')
+)
