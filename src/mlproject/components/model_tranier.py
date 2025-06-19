7b4410ddbfb1519469ff2c60422729ddbef1fc11
SRINU NAYAK <srinunayakk7@gmail.com>
2025-06-19 09:21:41 -0400

    found the best model

--- src/mlproject/components/model_tranier.py
+++ src/mlproject/components/model_tranier.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import r2_score
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
@@ -9,7 +10,7 @@ import os
 from pathlib import Path
 import sys
 from sklearn.linear_model import LinearRegression
-from src.mlproject.utils import evaluate_models
+from src.mlproject.utils import evaluate_models, save_object
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
@@ -45,17 +46,79 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
 
-            model_report = evaluate_models (
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+
+            }
+
+            model_report, trained_models, best_params = evaluate_models (
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models= models
+                models= models,
+                params = params
             )
 
             best_model_score = max(sorted(model_report.values()))
-            print(best_model_score)
+            logging.info('best_model_score: {}'.format(best_model_score))
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = trained_models[best_model_name]
+            best_model_hyperparams = best_params[best_model_name]
+
+            print(best_model)
+            print(best_model_hyperparams)
+
+
+
+            if best_model_score < 0.6:
+                raise CustomException('Best model score is too low')
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
 
         except Exception as e:
             raise CustomException(e, sys)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,5 +1,8 @@
 import pickle
 import sys
+
+from sklearn.model_selection import GridSearchCV
+
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import mysql.connector
@@ -47,16 +50,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models:dict):
+def evaluate_models(X_train, y_train, X_test, y_test, models:dict, params):
     global r2_score
     try:
         results = {}
+        trained_models = {}
+        best_params = {}
+
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_test)
-            r2 = r2_score(y_test, y_pred)
-            results[model_name] = r2
-        return results
+
+            param_grid = params.get(model_name, {})
+            # print(param_grid)
+
+            gs = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='r2')
+            gs.fit(X_train, y_train)
+
+            # Predict using the best estimator
+            best_model = gs.best_estimator_
+            y_pred = best_model.predict(X_test)
+
+            test_r2 = r2_score(y_test, y_pred)
+
+            results[model_name] = test_r2
+            trained_models[model_name] = best_model
+            best_params[model_name] = gs.best_params_
+
+        return results, trained_models, best_params
 
     except Exception as e:
         raise CustomException(e, sys)
+
+# def save_object(file_path, obj):
+#     try:
+#         dir_path = os.path.dirname(file_path)
+#         os.makedirs(dir_path, exist_ok=True)
+#         with open(file_path, 'wb') as f:
+#             pickle.dump(obj, f)
+#     except Exception as e:
+#         raise CustomException(e, sys)
